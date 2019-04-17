de70d69574a955a39066bb9bb2a194de7b03d78a
Georgi Berchev <g.berchev@gmail.com>
2019-04-17 13:57:21 +0300

    Fixing code in chapter 5 prod

--- chapter_5/zero_downtime_deployment/live/prod/services/webserver-cluster/main.tf
+++ chapter_5/zero_downtime_deployment/live/prod/services/webserver-cluster/main.tf
@@ -7,12 +7,11 @@ module "webserver_cluster" {
 
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "berchev-terraform-book-state"
-  db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
+  db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
   server_port            = 8080
 
   instance_type        = "t2.micro"
   min_size             = 2
   max_size             = 10
   enable_autoscaling   = true
-  enable_new_user_data = false
 }
